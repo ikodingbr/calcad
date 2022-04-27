2b4606ff71063877f66de7f979d39fcb6f92a194
Gabriel G. Freitas <67321807+gabrielgoncalfreitas@users.noreply.github.com>
2022-04-26 22:14:20 -0300

    main

--- js/script.js
+++ js/script.js
@@ -1,39 +1,52 @@
+var isApresentationHidden = false
+var isCurvasHidden = true
+var isCurva90RaioCurtoInoxHidden = true
+var isCurva45RaioCurtoInoxHidden = true
+var isCurva90RaioLongoInoxHidden = true
+var isCurva90RaioCurtoCarbonoHidden = true
+var isCurva45RaioCurtoCarbonoHidden = true
+var isCurva90RaioCLongoCarbonoHidden = true
+
 $(document).ready(function () {
+    //Set value 0
     $('#sch5sCurva90RaioCurtoInox').val((0 * 5).toFixed(3).replace('.', ',') + ' KG')
     $('#sch10sCurva90RaioCurtoInox').val((0 * 6.35).toFixed(3).replace('.', ',') + ' KG')
     $('#esp8Curva90RaioCurtoInox').val((0 * 8).toFixed(3).replace('.', ',') + ' KG')
     $('#stdsCurva90RaioCurtoInox').val((0 * 9.53).toFixed(3).replace('.', ',') + ' KG')
     $('#xssCurva90RaioCurtoInox').val((0 * 12.7).toFixed(3).replace('.', ',') + ' KG')
-    
+
     $('#sch5sCurva45RaioCurtoInox').val((0 * 5).toFixed(3).replace('.', ',') + ' KG')
     $('#sch10sCurva45RaioCurtoInox').val((0 * 6.35).toFixed(3).replace('.', ',') + ' KG')
     $('#esp8Curva45RaioCurtoInox').val((0 * 8).toFixed(3).replace('.', ',') + ' KG')
     $('#stdsCurva45RaioCurtoInox').val((0 * 9.53).toFixed(3).replace('.', ',') + ' KG')
     $('#xssCurva45RaioCurtoInox').val((0 * 12.7).toFixed(3).replace('.', ',') + ' KG')
-    
+
     $('#sch5sCurva90RaioLongoInox').val((0 * 5).toFixed(3).replace('.', ',') + ' KG')
     $('#sch10sCurva90RaioLongoInox').val((0 * 6.35).toFixed(3).replace('.', ',') + ' KG')
     $('#esp8Curva90RaioLongoInox').val((0 * 8).toFixed(3).replace('.', ',') + ' KG')
     $('#stdsCurva90RaioLongoInox').val((0 * 9.53).toFixed(3).replace('.', ',') + ' KG')
     $('#xssCurva90RaioLongoInox').val((0 * 12.7).toFixed(3).replace('.', ',') + ' KG')
-    
+
     $('#sch5sCurva90RaioCurtoAc').val((0 * 5).toFixed(3).replace('.', ',') + ' KG')
     $('#sch10sCurva90RaioCurtoAc').val((0 * 6.35).toFixed(3).replace('.', ',') + ' KG')
     $('#esp8Curva90RaioCurtoAc').val((0 * 8).toFixed(3).replace('.', ',') + ' KG')
     $('#stdsCurva90RaioCurtoAc').val((0 * 9.53).toFixed(3).replace('.', ',') + ' KG')
     $('#xssCurva90RaioCurtoAc').val((0 * 12.7).toFixed(3).replace('.', ',') + ' KG')
-    
+
     $('#sch5sCurva45RaioCurtoAc').val((0 * 5).toFixed(3).replace('.', ',') + ' KG')
     $('#sch10sCurva45RaioCurtoAc').val((0 * 6.35).toFixed(3).replace('.', ',') + ' KG')
     $('#esp8Curva45RaioCurtoAc').val((0 * 8).toFixed(3).replace('.', ',') + ' KG')
     $('#stdsCurva45RaioCurtoAc').val((0 * 9.53).toFixed(3).replace('.', ',') + ' KG')
     $('#xssCurva45RaioCurtoAc').val((0 * 12.7).toFixed(3).replace('.', ',') + ' KG')
-    
+
     $('#sch5sCurva90RaioLongoAc').val((0 * 5).toFixed(3).replace('.', ',') + ' KG')
     $('#sch10sCurva90RaioLongoAc').val((0 * 6.35).toFixed(3).replace('.', ',') + ' KG')
     $('#esp8Curva90RaioLongoAc').val((0 * 8).toFixed(3).replace('.', ',') + ' KG')
     $('#stdsCurva90RaioLongoAc').val((0 * 9.53).toFixed(3).replace('.', ',') + ' KG')
     $('#xssCurva90RaioLongoAc').val((0 * 12.7).toFixed(3).replace('.', ',') + ' KG')
+
+    //Hide
+    $('#containerCurvas').hide()
 });
 
 function calcCurva90RaioCurtoInox() {
@@ -112,4 +125,259 @@ function calcCurva90RaioLongoAc() {
     $('#esp8Curva90RaioLongoAc').val((res * 8).toFixed(3).replace('.', ',') + ' KG')
     $('#stdsCurva90RaioLongoAc').val((res * 9.53).toFixed(3).replace('.', ',') + ' KG')
     $('#xssCurva90RaioLongoAc').val((res * 12.7).toFixed(3).replace('.', ',') + ' KG')
+}
+
+function Curva90RaioCurtoInox() {
+    if (isCurva90RaioCurtoInoxHidden == true) {
+        $('#apresentation').fadeOut(150)
+        $('#containerCurva45RaioCurtoInox').fadeOut(150)
+        $('#containerCurva90RaioLongoInox').fadeOut(150)
+        $('#containerCurva90RaioCurtoCarbono').fadeOut(150)
+        $('#containerCurva45RaioCurtoCarbono').fadeOut(150)
+        $('#containerCurva90RaioLongoCarbono').fadeOut(150)
+
+        isApresentationHidden = true
+        isCurva45RaioCurtoInoxHidden = true
+        isCurva90RaioLongoInoxHidden = true
+        isCurva90RaioCurtoCarbonoHidden = true
+        isCurva45RaioCurtoCarbonoHidden = true
+        isCurva90RaioCLongoCarbonoHidden = true
+
+        isCurva90RaioCurtoInoxHidden = false
+
+        $('#containerCurvas').fadeIn(1000)
+        $('#containerCurva90RaioCurtoInox').fadeIn(1000)
+    }
+    else {
+        $('#containerCurva90RaioCurtoInox').fadeOut(150)
+        $('#containerCurva45RaioCurtoInox').fadeOut(150)
+        $('#containerCurva90RaioLongoInox').fadeOut(150)
+        $('#containerCurva90RaioCurtoCarbono').fadeOut(150)
+        $('#containerCurva45RaioCurtoCarbono').fadeOut(150)
+        $('#containerCurva90RaioLongoCarbono').fadeOut(150)
+
+        isCurva90RaioCurtoInoxHidden = true
+        isCurva45RaioCurtoInoxHidden = true
+        isCurva90RaioLongoInoxHidden = true
+        isCurva90RaioCurtoCarbonoHidden = true
+        isCurva45RaioCurtoCarbonoHidden = true
+        isCurva90RaioCLongoCarbonoHidden = true
+
+        isApresentationHidden = false
+
+        $('#apresentation').show(1000)
+    }
+}
+
+function Curva45RaioCurtoInox() {
+    if (isCurva45RaioCurtoInoxHidden == true) {
+        $('#apresentation').fadeOut(150)
+        $('#containerCurva90RaioCurtoInox').fadeOut(150)
+        $('#containerCurva90RaioLongoInox').fadeOut(150)
+        $('#containerCurva90RaioCurtoCarbono').fadeOut(150)
+        $('#containerCurva45RaioCurtoCarbono').fadeOut(150)
+        $('#containerCurva90RaioLongoCarbono').fadeOut(150)
+
+        isApresentationHidden = true
+        isCurva90RaioCurtoInoxHidden = true
+        isCurva90RaioLongoInoxHidden = true
+        isCurva90RaioCurtoCarbonoHidden = true
+        isCurva45RaioCurtoCarbonoHidden = true
+        isCurva90RaioCLongoCarbonoHidden = true
+
+        isCurva45RaioCurtoInoxHidden = false
+
+        $('#containerCurvas').fadeIn(1000)
+        $('#containerCurva45RaioCurtoInox').fadeIn(1000)
+    }
+    else {
+        $('#containerCurva90RaioCurtoInox').fadeOut(150)
+        $('#containerCurva45RaioCurtoInox').fadeOut(150)
+        $('#containerCurva90RaioLongoInox').fadeOut(150)
+        $('#containerCurva90RaioCurtoCarbono').fadeOut(150)
+        $('#containerCurva45RaioCurtoCarbono').fadeOut(150)
+        $('#containerCurva90RaioLongoCarbono').fadeOut(150)
+
+        isCurva90RaioCurtoInoxHidden = true
+        isCurva45RaioCurtoInoxHidden = true
+        isCurva90RaioLongoInoxHidden = true
+        isCurva90RaioCurtoCarbonoHidden = true
+        isCurva45RaioCurtoCarbonoHidden = true
+        isCurva90RaioCLongoCarbonoHidden = true
+
+        isApresentationHidden = false
+
+        $('#apresentation').show(1000)
+    }
+}
+
+function Curva90RaioLongoInox() {
+    if (isCurva90RaioLongoInoxHidden == true) {
+        $('#apresentation').fadeOut(150)
+        $('#containerCurva90RaioCurtoInox').fadeOut(150)
+        $('#containerCurva45RaioCurtoInox').fadeOut(150)
+        $('#containerCurva90RaioCurtoCarbono').fadeOut(150)
+        $('#containerCurva45RaioCurtoCarbono').fadeOut(150)
+        $('#containerCurva90RaioLongoCarbono').fadeOut(150)
+
+        isApresentationHidden = true
+        isCurva90RaioCurtoInoxHidden = true
+        isCurva45RaioCurtoInoxHidden = true
+        isCurva90RaioCurtoCarbonoHidden = true
+        isCurva45RaioCurtoCarbonoHidden = true
+        isCurva90RaioCLongoCarbonoHidden = true
+
+        isCurva90RaioLongoInoxHidden = false
+
+        $('#containerCurvas').fadeIn(1000)
+        $('#containerCurva90RaioLongoInox').fadeIn(1000)
+
+    }
+    else {
+        $('#containerCurva90RaioCurtoInox').fadeOut(150)
+        $('#containerCurva45RaioCurtoInox').fadeOut(150)
+        $('#containerCurva90RaioLongoInox').fadeOut(150)
+        $('#containerCurva90RaioCurtoCarbono').fadeOut(150)
+        $('#containerCurva45RaioCurtoCarbono').fadeOut(150)
+        $('#containerCurva90RaioLongoCarbono').fadeOut(150)
+
+        isCurva90RaioCurtoInoxHidden = true
+        isCurva45RaioCurtoInoxHidden = true
+        isCurva90RaioLongoInoxHidden = true
+        isCurva90RaioCurtoCarbonoHidden = true
+        isCurva45RaioCurtoCarbonoHidden = true
+        isCurva90RaioCLongoCarbonoHidden = true
+
+        isApresentationHidden = false
+
+        $('#apresentation').show(1000)
+    }
+}
+
+function Curva90RaioCurtoCarbono() {
+    if (isCurva90RaioCurtoCarbonoHidden == true) {
+        $('#apresentation').fadeOut(150)
+        $('#containerCurva90RaioCurtoInox').fadeOut(150)
+        $('#containerCurva45RaioCurtoInox').fadeOut(150)
+        $('#containerCurva90RaioLongoInox').fadeOut(150)
+        $('#containerCurva45RaioCurtoCarbono').fadeOut(150)
+        $('#containerCurva90RaioLongoCarbono').fadeOut(150)
+
+        isApresentationHidden = true
+        isCurva90RaioCurtoInoxHidden = true
+        isCurva45RaioCurtoInoxHidden = true
+        isCurva90RaioLongoInoxHidden = true
+        isCurva45RaioCurtoCarbonoHidden = true
+        isCurva90RaioCLongoCarbonoHidden = true
+
+        isCurva90RaioCurtoCarbonoHidden = false
+
+        $('#containerCurvas').fadeIn(1000)
+        $('#containerCurva90RaioCurtoCarbono').fadeIn(1000)
+
+    }
+    else {
+        $('#containerCurva90RaioCurtoInox').fadeOut(150)
+        $('#containerCurva45RaioCurtoInox').fadeOut(150)
+        $('#containerCurva90RaioLongoInox').fadeOut(150)
+        $('#containerCurva90RaioCurtoCarbono').fadeOut(150)
+        $('#containerCurva45RaioCurtoCarbono').fadeOut(150)
+        $('#containerCurva90RaioLongoCarbono').fadeOut(150)
+
+        isCurva90RaioCurtoInoxHidden = true
+        isCurva45RaioCurtoInoxHidden = true
+        isCurva90RaioLongoInoxHidden = true
+        isCurva90RaioCurtoCarbonoHidden = true
+        isCurva45RaioCurtoCarbonoHidden = true
+        isCurva90RaioCLongoCarbonoHidden = true
+
+        isApresentationHidden = false
+
+        $('#apresentation').show(1000)
+    }
+}
+
+function Curva45RaioCurtoCarbono() {
+    if (isCurva45RaioCurtoCarbonoHidden == true) {
+        $('#apresentation').fadeOut(150)
+        $('#containerCurva90RaioCurtoInox').fadeOut(150)
+        $('#containerCurva45RaioCurtoInox').fadeOut(150)
+        $('#containerCurva90RaioLongoInox').fadeOut(150)
+        $('#containerCurva90RaioCurtoCarbono').fadeOut(150)
+        $('#containerCurva90RaioLongoCarbono').fadeOut(150)
+
+        isApresentationHidden = true
+        isCurva90RaioCurtoInoxHidden = true
+        isCurva45RaioCurtoInoxHidden = true
+        isCurva90RaioLongoInoxHidden = true
+        isCurva90RaioCurtoCarbonoHidden = true
+        isCurva90RaioCLongoCarbonoHidden = true
+
+        isCurva45RaioCurtoCarbonoHidden = false
+
+        $('#containerCurvas').fadeIn(1000)
+        $('#containerCurva45RaioCurtoCarbono').fadeIn(1000)
+
+    }
+    else {
+        $('#containerCurva90RaioCurtoInox').fadeOut(150)
+        $('#containerCurva45RaioCurtoInox').fadeOut(150)
+        $('#containerCurva90RaioLongoInox').fadeOut(150)
+        $('#containerCurva90RaioCurtoCarbono').fadeOut(150)
+        $('#containerCurva45RaioCurtoCarbono').fadeOut(150)
+        $('#containerCurva90RaioLongoCarbono').fadeOut(150)
+
+        isCurva90RaioCurtoInoxHidden = true
+        isCurva45RaioCurtoInoxHidden = true
+        isCurva90RaioLongoInoxHidden = true
+        isCurva90RaioCurtoCarbonoHidden = true
+        isCurva45RaioCurtoCarbonoHidden = true
+        isCurva90RaioCLongoCarbonoHidden = true
+
+        isApresentationHidden = false
+
+        $('#apresentation').show(1000)
+    }
+}
+
+function Curva90RaioLongoCarbono() {
+    if (isCurva90RaioCLongoCarbonoHidden == true) {
+        $('#apresentation').fadeOut(150)
+        $('#containerCurva90RaioCurtoInox').fadeOut(150)
+        $('#containerCurva45RaioCurtoInox').fadeOut(150)
+        $('#containerCurva90RaioLongoInox').fadeOut(150)
+        $('#containerCurva90RaioCurtoCarbono').fadeOut(150)
+        $('#containerCurva45RaioCurtoCarbono').fadeOut(150)
+
+        isApresentationHidden = true
+        isCurva90RaioCurtoInoxHidden = true
+        isCurva45RaioCurtoInoxHidden = true
+        isCurva90RaioLongoInoxHidden = true
+        isCurva90RaioCurtoCarbonoHidden = true
+        isCurva45RaioCurtoCarbonoHidden = true
+
+        isCurva90RaioCLongoCarbonoHidden = false
+
+        $('#containerCurvas').fadeIn(1000)
+        $('#containerCurva90RaioLongoCarbono').fadeIn(1000)
+    }
+    else {
+        $('#containerCurva90RaioCurtoInox').fadeOut(150)
+        $('#containerCurva45RaioCurtoInox').fadeOut(150)
+        $('#containerCurva90RaioLongoInox').fadeOut(150)
+        $('#containerCurva90RaioCurtoCarbono').fadeOut(150)
+        $('#containerCurva45RaioCurtoCarbono').fadeOut(150)
+        $('#containerCurva90RaioLongoCarbono').fadeOut(150)
+
+        isCurva90RaioCurtoInoxHidden = true
+        isCurva45RaioCurtoInoxHidden = true
+        isCurva90RaioLongoInoxHidden = true
+        isCurva90RaioCurtoCarbonoHidden = true
+        isCurva45RaioCurtoCarbonoHidden = true
+        isCurva90RaioCLongoCarbonoHidden = true
+
+        isApresentationHidden = false
+
+        $('#apresentation').show(1000)
+    }
 }
