0c55cbe5ecff599fd092e911e044870dc564db38
gabrielgoncalfreitas <gabriel.goncal.freitas@gmail.com>
2022-04-29 23:16:46 -0300

    main

--- js/script.js
+++ js/script.js
@@ -8,6 +8,14 @@ var pi = parseFloat(3.14);
 $(document).ready(function () {
   $('.resultButton').val(parseFloat(0).toFixed(2) + ' KG');
   $('.modalWindow').hide();
+
+  $('.toggleMaterial').css('background-color', 'rgb(10, 10, 10)');
+  $('.toggleMaterial').css('color', 'rgb(255, 255, 255)');
+
+  $('.modalWindow').css('border', '1px solid rgb(255, 255, 255)');
+
+  $('input').css('background-color', 'rgb(10, 10, 10)');
+  $('input').css('color', 'rgb(255, 255, 255)');
 });
 
 function ShowHide(modal) {
@@ -27,15 +35,21 @@ function refresh() {
 
 function toggleDarkMode() {
   if ($('.toggleMaterial').css('background-color') == 'rgb(255, 255, 255)') {
-    $('.toggleMaterial').css('background-color', 'rgb(46, 46, 46)');
+    $('.toggleMaterial').css('background-color', 'rgb(10, 10, 10)');
     $('.toggleMaterial').css('color', 'rgb(255, 255, 255)');
 
     $('.modalWindow').css('border', '1px solid rgb(255, 255, 255)');
+
+    $('input').css('background-color', 'rgb(10, 10, 10)');
+    $('input').css('color', 'rgb(255, 255, 255)');
   }
-  else if ($('.toggleMaterial').css('background-color') == 'rgb(46, 46, 46)') {
+  else if ($('.toggleMaterial').css('background-color') == 'rgb(10, 10, 10)') {
     $('.toggleMaterial').css('background-color', 'rgb(255, 255, 255)');
-    $('.toggleMaterial').css('color', 'rgb(46, 46, 46)');
+    $('.toggleMaterial').css('color', 'rgb(10, 10, 10)');
 
     $('.modalWindow').css('border', '1px solid rgb(0, 0, 0)');
+
+    $('input').css('background-color', 'rgb(255, 255, 255)');
+    $('input').css('color', 'rgb(0, 0, 0)');
   };
 };
